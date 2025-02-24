9771049f7925d0e2eef757e6ac627c2b51cf13f0
Keyru Nasir <keyrsyntax@gmail.com>
2025-02-24 12:00:17 +0300

    Update models

---
+++ backend/models/pharmacyProductsModel.ts
@@ -0,0 +1,96 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../config/dbMySQLconfig";
+
+
+export interface pharmacyUserInterface {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  phoneNumber: number;
+  role: string;
+  isBlocked: string;
+isSoftDeleted:boolean;
+}
+
+class pharmacyUser
+  extends Model<pharmacyUserInterface>
+  implements pharmacyUserInterface
+{
+  public id!: number;
+  public firstName!: string;
+  public lastName!: string;
+  public email!: string;
+  public password!: string;
+  public phoneNumber!: number;
+  public role!: string;
+  public isBlocked!: string;
+  public isSoftDeleted!:boolean;
+}
+pharmacyUser.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "pharmacist",
+    },
+    isBlocked: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "Not Blocked",
+    },
+    isSoftDeleted:{
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+
+}
+  },
+  {
+    sequelize,
+    modelName: "pharmacyUser",
+    tableName: "pharmacyUser",
+    hooks: {
+      beforeCreate: async (user) => {
+        user.password = await bcrypt.hash(user.password, 10);
+      },
+    },
+    timestamps: true,
+  }
+);
+
+export default pharmacyUser;
