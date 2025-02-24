ca87a49298db3094dbe59f0decc829e7ca7e1766
Keyru Nasir <keyrsyntax@gmail.com>
2025-02-24 11:21:11 +0300

    Update pharmacyUserModel.ts

--- backend/models/pharmacyUserModel.ts
+++ backend/models/pharmacyUserModel.ts
@@ -11,6 +11,7 @@ export interface pharmacyUserInterface {
   phoneNumber: number;
   role: string;
   isBlocked: string;
+isSoftDeleted:boolean;
 }
 
 class pharmacyUser
@@ -25,6 +26,7 @@ class pharmacyUser
   public phoneNumber!: number;
   public role!: string;
   public isBlocked!: string;
+  public isSoftDeleted!:boolean;
 }
 pharmacyUser.init(
   {
