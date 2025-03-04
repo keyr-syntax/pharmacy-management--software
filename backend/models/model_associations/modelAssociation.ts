60e335d35f70a40f1aa9bbc19b264fefc11ca2f6
Keyru Nasir <keyrsyntax@gmail.com>
2025-03-04 13:25:29 +0300

    Update modelAssociation.ts

--- backend/models/model_associations/modelAssociation.ts
+++ backend/models/model_associations/modelAssociation.ts
@@ -1,6 +1,8 @@
 import DRUGS from "../drug_model/drugsModel";
 import DRUG_MANUFACTURERS from "../drug_model/drugManufacturersModel";
 import DRUG_INVENTORY from "../drug_model/drugInventory";
+import DRUG_PRICING from "../drug_model/drugPricingModel";
+
 
 const modelAssociation = () => {
   try {
@@ -20,6 +22,22 @@ const modelAssociation = () => {
       foreignKey: "drugID",
       as: "drug",
     });
+DRUGS.hasOne(DRUG_PRICING,{
+foreignKey: "drugID",
+      as: "pricing",
+}
+);
+
+DRUG_PRICING.belingsTo(DRUGS,{
+foreignKey: "drugID",
+      as: "drug",
+}
+);
+
+
+
+
+
   } catch (error) {
     console.log("Error while creating model association", error);
   }
