3de2fdba4bda196471ee1279d5a6d10241bff3eb
Keyru Nasir <keyrsyntax@gmail.com>
2025-03-04 14:37:12 +0300

    Update drugPricingController.ts

--- backend/controllers/drug_controllers/drugPricingController.ts
+++ backend/controllers/drug_controllers/drugPricingController.ts
@@ -109,4 +109,233 @@ export const findAllDrugPricing = async (req: Request, res: Response) => {
       message: "Failed to find drug pricing",
     });
   }
-}; 
+};
+
+export const findPricingByUUID = async (req: Request, res: Response) => {
+  try {
+    const { pricingID, drugID } = req.params;
+    const pricingByPK = await DRUG_PRICING.findByPk(pricingID, {
+      include: {
+        model: DRUGS,
+        as: "drug",
+        where: {
+          drugID: drugID,
+        },
+      },
+    });
+
+    if (pricingByPK && !pricingByPK.softDeleted) {
+      res.status(200).json({
+        success: true,
+        pricing: pricingByPK,
+      });
+      return;
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "Pricing not found",
+      });
+      return;
+    }
+  } catch (error) {
+    console.log("Error while finding Pricing", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to find Pricing by UUID",
+    });
+  }
+};
+
+export const updateDrugPricing = async (req: Request, res: Response) => {
+  try {
+    const { pricingID } = req.params;
+    const {
+      drugID,
+      purchasePrice,
+      sellingPrice,
+      taxRate,
+      margin,
+      insuranceCoverage,
+    } = req.body;
+    const pricingByPK = await DRUG_PRICING.findByPk(pricingID);
+
+    const isPricingTheSame =
+      pricingByPK?.drugID === drugID &&
+      pricingByPK?.purchasePrice === purchasePrice &&
+      pricingByPK?.sellingPrice === sellingPrice &&
+      pricingByPK?.taxRate === taxRate &&
+      pricingByPK?.margin === margin &&
+      pricingByPK?.insuranceCoverage === insuranceCoverage;
+
+    if (pricingByPK && !pricingByPK.softDeleted && !isPricingTheSame) {
+      const updatePricing = await pricingByPK.update({
+        drugID: drugID,
+        purchasePrice: purchasePrice,
+        sellingPrice: sellingPrice,
+        taxRate: taxRate,
+        margin: margin,
+        insuranceCoverage: insuranceCoverage,
+      });
+      const findAllDrugPricing = await DRUG_PRICING.findAll({
+        where: { softDeleted: false },
+        order: [["createdAt", "ASC"]],
+      });
+      if (updatePricing) {
+        res.status(200).json({
+          success: true,
+          pricing: updatePricing,
+          allPricing: findAllDrugPricing,
+          message: "Pricing updated",
+        });
+        return;
+      } else {
+        res.status(404).json({
+          success: true,
+          message: "Failed to update pricing",
+        });
+        return;
+      }
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "You have made no changes",
+      });
+      return;
+    }
+  } catch (error) {
+    console.log("Error while updating pricing", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to update pricing",
+    });
+  }
+};
+
+export const deleteDrugPricing = async (req: Request, res: Response) => {
+  try {
+    const { pricingID } = req.params;
+    const pricingByPK = await DRUG_PRICING.findOne({
+      where: {
+        pricingID: pricingID,
+        softDeleted: false,
+      },
+    });
+
+    if (pricingByPK) {
+      const softDeletePricing = await pricingByPK.update({
+        softDeleted: true,
+      });
+      const findAllDrugPricing = await DRUG_PRICING.findAll({
+        where: { softDeleted: false },
+        order: [["createdAt", "ASC"]],
+      });
+      if (softDeletePricing) {
+        res.status(200).json({
+          success: true,
+          message: "Deleted",
+          allPricing: findAllDrugPricing,
+        });
+        return;
+      } else {
+        res.status(200).json({
+          success: true,
+          message: "Failed to delete pricing",
+        });
+        return;
+      }
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "Pricing not found",
+      });
+      return;
+    }
+  } catch (error) {
+    console.log("Error while deleting pricing", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to delete pricing",
+    });
+  }
+};
+
+export const undoDeletedDrugPricing = async (req: Request, res: Response) => {
+  const { pricingID } = req.params;
+  try {
+    const pricingByID = await DRUG_PRICING.findOne({
+      where: {
+        pricingID: pricingID,
+        softDeleted: true,
+      },
+    });
+
+    if (pricingByID) {
+      const undoSoftDeletedPricing = await pricingByID.update({
+        softDeleted: false,
+      });
+
+      if (undoSoftDeletedPricing) {
+        const findAllPricing = await DRUG_PRICING.findAll({
+          where: {
+            softDeleted: true,
+          },
+          order: [["createdAt", "ASC"]],
+        });
+        res.status(200).json({
+          success: true,
+          message: "Pricing restored",
+          allPricing: findAllPricing,
+        });
+        return;
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "Failed to restore Pricing",
+        });
+        return;
+      }
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "Pricing not found",
+      });
+      return;
+    }
+  } catch (error) {
+    console.log("Error while restoring Pricing", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to restore Pricing",
+    });
+  }
+};
+
+export const fetchAllDeletedDrugPricing = async (req: Request, res: Response) => {
+  try {
+    const allDeletedPricing = await DRUG_PRICING.findAll({
+      where: {
+        softDeleted: true,
+      },
+      order: [["createdAt", "ASC"]],
+    });
+
+    if (allDeletedPricing) {
+      res.status(200).json({
+        success: true,
+        allPricing: allDeletedPricing,
+      });
+      return;
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "No deleted pricing",
+      });
+      return;
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch deleted pricing",
+    });
+  }
+};
